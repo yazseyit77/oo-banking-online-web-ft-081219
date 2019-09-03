07841dffa3bb64ae2ea38b42b326b67b77dc2aad
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-03 22:34:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
         @receiver.balance += @amount
         @status = "complete"
      else
-       "Transaction rejected. Please check your account balance."
+       puts "Transaction rejected. Please check your account balance."
      end
   end
 

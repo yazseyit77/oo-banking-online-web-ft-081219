1102a1ff92b43ce5779e0c7500cb36be2f9b0b9f
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-03 22:43:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
 
   def execute_transaction
     if @sender.balance > @amount && @status == "pending"
-#@sender.valid? && @status != "complete" && @sender.balance > @amount 
+#@sender.valid? && @status != "complete" && @sender.balance > @amount
         @sender.balance -= @amount
         @receiver.balance += @amount
         @status = "complete"

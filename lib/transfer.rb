f0199cd3966ee7ae222fdf82a981faf8032af58e
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-03 22:48:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,30 +15,30 @@ class Transfer
     sender.valid? && receiver.valid? ? true : false
   end
 
-  # def execute_transaction
-  #   if @sender.valid? && @status != "complete" && @sender.balance > @amount
-  #       @sender.balance -= @amount
-  #       @receiver.balance += @amount
-  #       @status = "complete"
-  #    else
-  #       @status = "rejected"
-  #       return "Transaction rejected. Please check your account balance."
-  #    end
-  # end
-
   def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 )
-      @receiver.deposit( @amount )
-      @status = "complete"
-    end
+    if @sender.valid? && @status != "complete" && @sender.balance > @amount
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+     else
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+     end
   end
+  
+  # def execute_transaction
+  #   if @sender.balance < @amount
+  #     @status = "rejected"
+  #     return "Transaction rejected. Please check your account balance."
+  # 
+  #   elsif @status == "complete"
+  #     puts "Transaction was already excuted"
+  #   else
+  #     @sender.deposit( @amount * -1 )
+  #     @receiver.deposit( @amount )
+  #     @status = "complete"
+  #   end
+  # end
 
 
   def reverse_transfer
